4223e8f65039901db1fbd23e7e7a0afd566f8bfa
StefanoBranz <stefano.branz@gmail.com>
2021-06-17 22:31:41 -0300

    Created pages, cotext, routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import CharacterDetail from './pages/CharacterDetail';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Main } />
+        <Route path="/character/detail/:id" component={ CharacterDetail } />
       </Switch>
     </BrowserRouter>
   );
