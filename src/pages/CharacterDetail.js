5623fdcdd14e2811ee66624fcd4c689373e5bc0b
StefanoBranz <stefano.branz@gmail.com>
2021-06-18 15:22:15 -0300

    Saving because i will try another aproach

--- src/pages/CharacterDetail.js
+++ src/pages/CharacterDetail.js
@@ -1,14 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import BrakingBadContext from '../context/BrakingBadContext';
 import { useParams } from 'react-router-dom';
 
 export default function CharacterDetail() {
-  const { characters } = useContext(BrakingBadContext);
+  const { characters, favoriteCharacters, setFavoriteCharacters } = useContext(BrakingBadContext);
   const { id } = useParams();
-  const characterDetail = characters.find((character) => character.char_id === Number(id))
+  const characterDetail = characters.find((character) => character.char_id === Number(id));
+  
+  useEffect(() => {
+    localStorage.setItem('favoriteCharacters', JSON.stringify(favoriteCharacters));
+  }, [favoriteCharacters])
+  
+  function favorite_char() {
+    const favoriteCheckbox = document.getElementById('favorite_char');
+    if (favoriteCheckbox.checked) {
+    setFavoriteCharacters([...favoriteCharacters, characterDetail])
+  }
+  if (!favoriteCheckbox.checked) {
+    const newFavorites = favoriteCharacters.filter(chars => chars.char_id !== characterDetail.char_id);
+    setFavoriteCharacters(newFavorites);
+  }
+}
 
-  return (
-    <div>
+function isFavorite() {
+  const myFavorites = JSON.parse(localStorage.getItem('favoriteCharacters'));
+  console.log('favorites local', myFavorites);
+  // const favoriteExists = myFavorites.find((character) => character.char_id === characterDetail.char_id);
+  // if(favoriteExists) {
+    //   document.getElementById('favorite_char').setAttribute('checked', 'checked');      
+    // }
+  }
+
+return (
+  <div>
       {(!characters.length)
         ? <h1>Loading...</h1>
         : <div>
@@ -20,6 +44,14 @@ export default function CharacterDetail() {
           {characterDetail.occupation.map((occ => <h5 key={ occ }>{occ}</h5>))}
           <p>{characterDetail.portrayed}</p>
           <p>{characterDetail.status}</p>
+          <label htmlFor="favorite_char">
+            <input
+              type="checkbox"
+              name="favorite_char"
+              id="favorite_char"
+              onClick={ favorite_char }
+            />
+          </label>
         </div>
       }
     </div>
